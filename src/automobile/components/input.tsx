ab352e2eeaea1dc0bff3a9cd08edc72e78d480cd
BuildTools <unconfigured@null.spigotmc.org>
2024-07-30 19:46:19 +0200

    Work in progress

---
+++ src/automobile/components/input.tsx
@@ -0,0 +1,7 @@
+const [value, setValue] = React.useState(0);
+
+export function input() {
+    return (
+    <input type="number"></input>
+   );
+  }
---
+++ src/automobile/index.tsx
@@ -0,0 +1,11 @@
+import { Main } from "next/document";
+import { Button } from "./components/HP";
+
+
+
+export default function Page() {
+    return (<Main>
+        <Button text="Calcola">
+        </Button>
+    </Main>);
+}
